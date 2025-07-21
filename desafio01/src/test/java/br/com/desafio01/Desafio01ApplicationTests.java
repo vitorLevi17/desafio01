6997dcff1428c15d9a188cb016c5c3af88819701
vitorLevi17 <vitorlevimenezessantos@outlook.com>
2025-07-21 16:39:39 -0300

    Criação do projeto

---
+++ desafio01/src/test/java/br/com/desafio01/Desafio01ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafio01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
