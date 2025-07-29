641ac500e3a871341d4ebeba717b6576e92abec0
vitorLevi17 <vitorlevimenezessantos@outlook.com>
2025-07-29 10:29:40 -0300

    feat: mudar senha do usuário

--- desafio01/src/main/java/br/com/desafio01/controller/TokenController.java
+++ desafio01/src/main/java/br/com/desafio01/controller/TokenController.java
@@ -2,17 +2,20 @@ package br.com.desafio01.controller;
 
 import br.com.desafio01.dto.LoginDto;
 import br.com.desafio01.dto.LoginResponse;
+import br.com.desafio01.dto.MudarSenhaDto;
+import br.com.desafio01.entities.User;
 import br.com.desafio01.services.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.time.Instant;
 
 @RestController
@@ -50,5 +53,17 @@ public class TokenController {
         var jwtValue = jwtEncoder.encode(JwtEncoderParameters.from(clains)).getTokenValue();
         return ResponseEntity.ok(new LoginResponse(jwtValue,expiresIn));
     }
+    @PutMapping("/mudar-senha")
+    public ResponseEntity mudarSenha(@RequestBody MudarSenhaDto mudarSenhaDto){
+        User user = userService.findByUsername(mudarSenhaDto.username()).orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
 
+        if (mudarSenhaDto.password().equals(mudarSenhaDto.confirm_password())){
+            PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+            user.setPassword(passwordEncoder.encode(mudarSenhaDto.password()));
+            userService.saveUser(user);
+        }else {
+            return ResponseEntity.ok("Usuario ou senha incorreto");
+        }
+        return ResponseEntity.ok("Senha alterada com sucesso");
+    }
 }
---
+++ desafio01/src/main/java/br/com/desafio01/dto/MudarSenhaDto.java
@@ -0,0 +1,4 @@
+package br.com.desafio01.dto;
+
+public record MudarSenhaDto(String username, String password, String confirm_password) {
+}
